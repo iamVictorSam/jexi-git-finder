4c24c2842ba017bff49344daffacbde81345f138
iamVictorSam <iamvictorsam@gmail.com>
2022-02-01 23:13:34 +0100

    sagemode

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <>
-      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <h1 className="text-6xl mb-4">Jexi Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
         A React app to search GitHub profiles and see profile details.
       </p>
